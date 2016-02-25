7f17ffebd104452a763aeb5a66aaf92f92defee6
Luke Chavers <vmadman@gmail.com>
2016-02-25 11:47:52 -0600

    Create yum-install-common.sh

---
+++ vagrant/centos7/dev/v1/yum-install-common.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script installs a number of common yum packages that I like to
+# include on every CentOS Vagrant box; mostly for utility purposes.
+
+echo ""
+echo "[Provision-Script] Installing Common Utils & Libraries"
+echo ""
+yum install -y mlocate pico
