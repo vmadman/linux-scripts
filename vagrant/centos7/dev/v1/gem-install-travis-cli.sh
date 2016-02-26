ec4c578d3b611eb72c463492e5f72fefbd6cf0b3
Luke Chavers <vmadman@gmail.com>
2016-02-25 23:45:55 -0600

    Update gem-install-travis-cli.sh

--- vagrant/centos7/dev/v1/gem-install-travis-cli.sh
+++ vagrant/centos7/dev/v1/gem-install-travis-cli.sh
@@ -11,3 +11,8 @@ echo ""
 echo "[Provision-Script] Installing Travis CLI"
 echo ""
 gem install travis --no-rdoc --no-ri
+
+echo ""
+echo "[Provision-Script] Creating Symlink for Travis CLI"
+echo ""
+ln -s /usr/local/bin/travis /usr/bin/travis
