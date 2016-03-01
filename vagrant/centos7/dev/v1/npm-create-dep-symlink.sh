5604749314d905f506be6977804fc89086849296
Luke Chavers <vmadman@gmail.com>
2016-03-01 09:07:48 -0600

    Update npm-create-dep-symlink.sh
    Added dep symlink fix per https://github.com/nodejs/node-v0.x-archive/issues/25090#issuecomment-190752844

--- vagrant/centos7/dev/v1/npm-create-dep-symlink.sh
+++ vagrant/centos7/dev/v1/npm-create-dep-symlink.sh
@@ -5,11 +5,32 @@
 # to be located at /project).  This script is used to overcome
 # a Vagrant/Virtualbox issue with long paths on windows.
 
+NMSL_PROJECT_DIR="/project"
+NMSL_PROJECT_NM="$NMSL_PROJECT_DIR/node_modules"
+NMSL_STORE_PATH="/home/vagrant/node_modules"
+
 echo ""
 echo "[Provision-Script] Creating Symlink for node_modules"
 echo ""
 
-rm -rf /project/node_modules
-rm -rf /home/vagrant/node_modules/project
-mkdir -p /home/vagrant/node_modules/project
-ln -s /home/vagrant/node_modules/project /project/node_modules
+echo "   > Removing existing symlink (if it exists)"
+
+rm -rf "$NMSL_PROJECT_NM"
+
+echo "   > Removing existing node_modules store (if it exists)"
+
+rm -rf "$NMSL_STORE_PATH"
+
+echo "   > Creating new node_modules store"
+
+mkdir -p "$NMSL_STORE_PATH"
+
+echo "   > Creating new symlink"
+
+ln -s "$NMSL_STORE_PATH" "$NMSL_PROJECT_NM"
+
+echo "   > Applying permissions to node_modules store"
+
+chown vagrant:vagrant "$NMSL_STORE_PATH" -R
+
+echo ""
