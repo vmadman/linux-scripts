142410551bda30e0235a11cbeb992669a3de10da
Luke Chavers <vmadman@gmail.com>
2016-04-20 16:23:43 -0500

    Create npm-install-serverless.sh

---
+++ vagrant/centos7/dev/v1/npm-install-serverless.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script uses NPM to install the "Serverless" CLI globally
+
+echo ""
+echo "[Provision-Script] Installing the Serverless Framework"
+echo ""
+
+npm install -g serverless
