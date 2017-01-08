9afa8a7e38b80dd3927c1687cd99213bbc7db023
Luke Chavers <vmadman@gmail.com>
2017-01-08 12:47:12 -0600

    Create npm-install-containership.sh

---
+++ vagrant/centos7/dev/v1/npm-install-containership.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script uses NPM to install the ContainerShip CLI globally
+
+echo ""
+echo "[Provision-Script] Installing ContainerShip CLI"
+echo ""
+
+npm install -g containership
