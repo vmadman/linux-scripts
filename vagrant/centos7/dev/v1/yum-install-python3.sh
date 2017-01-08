fa3cfb285ca20365cb869a9e25cf9aff5fad2bb9
Luke Chavers <vmadman@gmail.com>
2017-01-08 13:43:53 -0600

    Create yum-install-python3.sh

---
+++ vagrant/centos7/dev/v1/yum-install-python3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script installs the latest stable version of Python 3.x
+
+echo ""
+echo "[Provision-Script] Installing Python 3.x"
+echo ""
+yum -y install python34 python34-pip
+python3 --version
