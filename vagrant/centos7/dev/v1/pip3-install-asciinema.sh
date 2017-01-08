88a05193496eae4c9c75dc55aa13783de39dc197
Luke Chavers <vmadman@gmail.com>
2017-01-08 13:46:25 -0600

    Create pip3-install-asciinema.sh

---
+++ vagrant/centos7/dev/v1/pip3-install-asciinema.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script installs Asciinema (https://asciinema.org/docs/installation)
+# requires: yum-install-python3.sh
+
+echo ""
+echo "[Provision-Script] Installing Asciinema"
+echo ""
+
+pip3 install asciinema
