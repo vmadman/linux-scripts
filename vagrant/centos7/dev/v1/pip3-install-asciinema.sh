f325ae78f3856ec5c87ab8cb78eff823bad98864
Luke Chavers <vmadman@gmail.com>
2017-01-08 13:59:46 -0600

    Update pip3-install-asciinema.sh

--- vagrant/centos7/dev/v1/pip3-install-asciinema.sh
+++ vagrant/centos7/dev/v1/pip3-install-asciinema.sh
@@ -8,3 +8,19 @@ echo "[Provision-Script] Installing Asciinema"
 echo ""
 
 pip3 install asciinema
+
+# Create acn symlink
+rm -rf /usr/bin/acn
+ln -s /bin/asciinema /usr/bin/acn
+
+# [root@containership project]# echo '#!/bin/bash' > /usr/bin/acn
+# [root@containership project]# cat /usr/bin/acn
+# #!/bin/bash
+# [root@containership project]# echo '' >> /usr/bin/acn
+# [root@containership project]# echo 'hi' >> /usr/bin/acn
+# [root@containership project]# cat /usr/bin/acn
+
+
+
+echo ""
+echo "  -- Asciinema has been installed -- "
