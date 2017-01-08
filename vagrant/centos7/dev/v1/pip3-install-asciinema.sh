2184b341fab801f232f99ec6eaa5fe3ae3df55b1
Luke Chavers <vmadman@gmail.com>
2017-01-08 14:13:26 -0600

    Update pip3-install-asciinema.sh

--- vagrant/centos7/dev/v1/pip3-install-asciinema.sh
+++ vagrant/centos7/dev/v1/pip3-install-asciinema.sh
@@ -9,9 +9,30 @@ echo ""
 
 pip3 install asciinema
 
+# Find install location
+ASCIINEMA_MAIN=`which asciinema`
+
+# Set up some basic vars
+ASCIINEMA_ALIAS_DIR="/usr/bin"
+ASCIINEMA_ALIAS_MAIN="$ASCIINEMA_ALIAS_DIR/acn"
+ASCIINEMA_ALIAS_AUTH="$ASCIINEMA_ALIAS_DIR/acna"
+ASCIINEMA_ALIAS_REC="$ASCIINEMA_ALIAS_DIR/acnr"
+
 # Create acn symlink
+echo "    - Remove existing symlinks ..."
+echo ""
 rm -rf /usr/bin/acn
-ln -s /bin/asciinema /usr/bin/acn
+
+
+# Create acn symlink
+echo "    - Creating 'acn' symlink ..."
+echo "        $ASCIINEMA_MAIN -> $ASCIINEMA_ALIAS_MAIN"
+echo ""
+ln -s "$ASCIINEMA_MAIN" "$ASCIINEMA_ALIAS_MAIN"
+
+# We'll grab the version as a test of the symlink
+ASCIINEMA_VERSION=`acn --version`
+
 
 # [root@containership project]# echo '#!/bin/bash' > /usr/bin/acn
 # [root@containership project]# cat /usr/bin/acn
@@ -20,10 +41,17 @@ ln -s /bin/asciinema /usr/bin/acn
 # [root@containership project]# echo 'hi' >> /usr/bin/acn
 # [root@containership project]# cat /usr/bin/acn
 
-ASCIINEMA_MAIN=`which asciinema`
+
 
 echo ""
-echo "  -- Asciinema has been installed 2 -- "
+echo "  -- Asciinema has been installed -- "
 echo ""
-echo "      Usage Docs: https://asciinema.org/docs/getting-started"
-echo "      Main Entrypoint: $ASCIINEMA_MAIN"
+echo "              Version : $ASCIINEMA_VERSION"
+echo "           Usage Docs : https://asciinema.org/docs/getting-started"
+echo "      Main Entrypoint : $ASCIINEMA_MAIN"
+echo ""
+echo "         Main Symlink : $ASCIINEMA_ALIAS_MAIN"
+echo "         Auth Symlink : $ASCIINEMA_ALIAS_AUTH"
+echo "       Record Symlink : $ASCIINEMA_ALIAS_REC"
+echo ""
+
