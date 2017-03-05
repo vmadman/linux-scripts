00bcc64006914cba61c3b10b2687851739708e84
Luke Chavers <vmadman@gmail.com>
2017-03-05 07:28:06 -0600

    Create npm-install-json-beautifier.sh

---
+++ vagrant/centos7/dev/v1/npm-install-json-beautifier.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script uses NPM to install the JSON Beautifier CLI utility
+# > https://github.com/andlrc/json-beautifier
+
+echo ""
+echo "[Provision-Script] Installing json-beautifier (via NPM)"
+echo ""
+
+npm install -g json-beautifier
