874ebd6a2aa48cf0cc962ab8b35fca5743ec0bc8
Luke Chavers <vmadman@gmail.com>
2017-05-13 17:08:41 -0500

    Update yum-install-yarn.sh

--- vagrant/centos7/dev/v1/yum-install-yarn.sh
+++ vagrant/centos7/dev/v1/yum-install-yarn.sh
@@ -9,4 +9,4 @@ echo ""
 
 wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-yum install yarn
+yum install yarn -y
