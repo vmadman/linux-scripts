3e80621cbf5edcd709767368225ae9e5e1c312e6
Luke Chavers <vmadman@gmail.com>
2017-05-13 17:06:16 -0500

    Create yum-install-yarn.sh

---
+++ vagrant/centos7/dev/v1/yum-install-yarn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script installs Yarn (A package manager that replaces NPM and/or Bower)
+# More info here: https://yarnpkg.com/en/
+
+echo ""
+echo "[Provision-Script] Installing Yarn"
+echo ""
+
+wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+yum install yarn
