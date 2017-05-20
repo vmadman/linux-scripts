fd8c3762219e9049bbfcb1387e15dd295e07bf8b
Luke Chavers <vmadman@gmail.com>
2017-05-20 05:15:51 -0500

    Create npm-install-claudia.sh

---
+++ vagrant/centos7/dev/v1/npm-install-claudia.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script uses NPM to install the "Claudia.js" CLI globally
+
+echo ""
+echo "[Provision-Script] Installing the Claudia.js CLI Utility"
+echo ""
+
+npm install -g claudia
