32b4ff9caa94e5ee23991b7dabb42bb468ff99c3
Luke Chavers <vmadman@gmail.com>
2017-06-04 03:54:14 -0500

    Update npm-install-livereloadx.sh

--- vagrant/centos7/dev/v1/npm-install-livereloadx.sh
+++ vagrant/centos7/dev/v1/npm-install-livereloadx.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+
+# Note: This app/solution is not as good as 'Browser Sync'..
+#       Consider using 'npm-install-browser-sync' instead.
+
+
+
 # This script uses NPM to install LiveReloadX globally
 
 echo ""
