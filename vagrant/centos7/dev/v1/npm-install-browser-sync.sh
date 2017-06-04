a674bbee6eb517926bb8fefadd2480923ab05e4c
Luke Chavers <vmadman@gmail.com>
2017-06-04 03:56:46 -0500

    Create npm-install-browser-sync.sh

---
+++ vagrant/centos7/dev/v1/npm-install-browser-sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script uses NPM to install "Browser Sync" globally
+# (This is superior, drop-in, replacement for LiveReloadX)
+#
+#      see: https://www.browsersync.io/
+#           https://www.browsersync.io/docs/command-line
+#           https://www.browsersync.io/docs/options
+#
+
+echo ""
+echo "[Provision-Script] Installing BrowserSync (Globally via NPM)"
+echo ""
+
+npm install -g browser-sync
