b3199463fbd11bb19333d0179bf30200eee4df1b
Luke Chavers <vmadman@gmail.com>
2017-06-04 00:51:04 -0500

    Create npm-install-ntl.sh

---
+++ vagrant/centos7/dev/v1/npm-install-ntl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script uses NPM to install NTL (Node Task List) globally
+# see: https://www.npmjs.com/package/ntl
+
+echo ""
+echo "[Provision-Script] Installing NTL (via NPM)"
+echo ""
+
+npm install -g ntl
