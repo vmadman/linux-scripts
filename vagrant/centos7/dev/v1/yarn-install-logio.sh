87072b09cb951151c94e795ced57518c05bcc532
Luke Chavers <vmadman@gmail.com>
2017-06-08 11:59:27 -0500

    Create yarn-install-logio.sh

---
+++ vagrant/centos7/dev/v1/yarn-install-logio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script uses Yarn to install Log.io globally
+# see: http://logio.org/
+# see: https://github.com/NarrativeScience/Log.io
+
+echo ""
+echo "[Provision-Script] Installing Log.io (via Yarn)"
+echo ""
+
+yarn global add logio
