2449d88b3741bb4c411285a5e931e77fceb39afb
Luke Chavers <vmadman@gmail.com>
2017-06-08 08:39:30 -0500

    Create yarn-install-ntl.sh

---
+++ vagrant/centos7/dev/v1/yarn-install-ntl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script uses Yarn to install NTL (Node Task List) globally
+# see: https://www.npmjs.com/package/ntl
+
+echo ""
+echo "[Provision-Script] Installing NTL (via Yarn)"
+echo ""
+
+yarn global add ntl
