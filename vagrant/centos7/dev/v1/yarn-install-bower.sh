58dbd10974f9be1b65fa895b8201452154a677ad
Luke Chavers <vmadman@gmail.com>
2017-06-20 07:08:14 -0500

    Create yarn-install-bower.sh

---
+++ vagrant/centos7/dev/v1/yarn-install-bower.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script uses Yarn to install Bower globally
+# see: https://bower.io/
+
+echo ""
+echo "[Provision-Script] Installing Bower (via Yarn)"
+echo ""
+
+yarn global add bower
