d47d0eebb7fb68bc83fa3a53646fe1cb82f997a5
Luke Chavers <vmadman@gmail.com>
2017-06-23 11:13:46 -0500

    Create yarn-install-hackmyresume.sh

---
+++ vagrant/centos7/dev/v1/yarn-install-hackmyresume.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script uses Yarn to install HackMyResume globally
+# see: https://please.hackmyresume.com/
+
+echo ""
+echo "[Provision-Script] Installing HackMyResume (via Yarn)"
+echo ""
+
+yarn global add hackmyresume
