4c2cb9972a4eb223a3e6de98c027e827a70b3b8d
Luke Chavers <vmadman@gmail.com>
2017-07-17 13:49:32 -0500

    Create yarn-install-serverless.sh

---
+++ vagrant/centos7/dev/v1/yarn-install-serverless.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script uses Yarn to install Serverless globally
+# see: https://serverless.com/
+
+echo ""
+echo "[Provision-Script] Installing Serverless (via Yarn)"
+echo ""
+
+yarn global add serverless
