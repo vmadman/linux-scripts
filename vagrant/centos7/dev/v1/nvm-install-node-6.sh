878710678548e7693ec09a6fb478c20908dbd7dd
Luke Chavers <vmadman@gmail.com>
2017-08-13 20:24:11 -0500

    Update nvm-install-node-6.sh

--- vagrant/centos7/dev/v1/nvm-install-node-6.sh
+++ vagrant/centos7/dev/v1/nvm-install-node-6.sh
@@ -2,19 +2,26 @@
 
 # This script installs NVM ("Node Version Manager") and
 # then the latest stable version of NodeJS 6.x.
+# - see: https://github.com/creationix/nvm#install-script
 
 echo ""
 echo "[Provision-Script] Installing Node.js Prerequisites"
 echo ""
-yum -y install gcc-c++ make
+#tmp:yum -y install gcc-c++ make
 
 echo ""
 echo "[Provision-Script] Installing NVM"
 echo ""
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+#tmp:curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+# Make NVM available, immediately, so that this script can use it
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo ""
 echo "[Provision-Script] Installing Node.js 6.x"
 echo ""
 echo "<todo>"
 echo ""
+nvm --version
