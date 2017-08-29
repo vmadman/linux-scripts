9ff6b6de52d110836482ed17f996eba5895d68ca
Luke Chavers <vmadman@gmail.com>
2017-08-29 18:53:37 -0500

    Create install-terraform.sh
    Added terraform installation (a work in progress)

---
+++ vagrant/centos7/dev/v1/install-terraform.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script installs Terraform (via a ZIP download from Terraform.io)
+#    see: https://www.terraform.io/intro/getting-started/install.html
+
+echo ""
+echo "[Provision-Script] Installing Terraform"
+echo ""
+
+# -- this is a work in progess! --
+
+#cd /root
+#rm -rf awscli-bundle
+#rm -f awscli-bundle.zip
+
+#curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+#unzip awscli-bundle.zip
+#./awscli-bundle/install -i /usr/local/aws -b /usr/bin/aws
+
+#https://releases.hashicorp.com/terraform/0.10.2/terraform_0.10.2_linux_amd64.zip?_ga=2.151879802.278059054.1504047112-1151940089.1504047112
