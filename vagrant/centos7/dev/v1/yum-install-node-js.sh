018dc275a46a96dceeca6210d3cecf30714435a1
Luke Chavers <vmadman@gmail.com>
2017-08-28 19:21:23 -0500

    Update yum-install-node-js.sh

--- vagrant/centos7/dev/v1/yum-install-node-js.sh
+++ vagrant/centos7/dev/v1/yum-install-node-js.sh
@@ -3,12 +3,17 @@
 # This script installs the latest stable version of NodeJS 4.x
 
 echo ""
-echo "[Provision-Script] Adding Yum Repo for NodeSource"
+echo "[Provision-Script] Node.js: Installing http-parser (temp workaround)"
+echo ""
+rpm -ivh https://kojipkgs.fedoraproject.org//packages/http-parser/2.7.1/3.el7/x86_64/http-parser-2.7.1-3.el7.x86_64.rpm
+
+echo ""
+echo "[Provision-Script] Node.js: Adding Yum Repo for NodeSource"
 echo ""
 curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 yum makecache fast
 
 echo ""
-echo "[Provision-Script] Installing Node.js"
+echo "[Provision-Script] Node.js: Installing Node.js"
 echo ""
 yum -y install nodejs gcc-c++ make
