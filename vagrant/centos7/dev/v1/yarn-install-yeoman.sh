f46f1484c60ae80b7336e1c815ee337ef8f7d7d1
Luke Chavers <vmadman@gmail.com>
2017-08-29 20:19:11 -0500

    Create yarn-install-yeoman.sh

---
+++ vagrant/centos7/dev/v1/yarn-install-yeoman.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script uses Yarn to install Yeoman globally
+#   see: http://yeoman.io/
+
+echo ""
+echo "[Provision-Script] Installing Yeoman (via Yarn)"
+echo ""
+
+yarn global add yo
