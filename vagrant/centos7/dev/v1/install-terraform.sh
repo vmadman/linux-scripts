90ad13f3b364158e57ac17f11c3546d4ad826f26
Luke Chavers <vmadman@gmail.com>
2017-08-29 22:18:25 -0500

    Update install-terraform.sh

--- vagrant/centos7/dev/v1/install-terraform.sh
+++ vagrant/centos7/dev/v1/install-terraform.sh
@@ -3,18 +3,50 @@
 # This script installs Terraform (via a ZIP download from Terraform.io)
 #    see: https://www.terraform.io/intro/getting-started/install.html
 
+TERRAFORM_VERSION="0.10.2"
+TERRAFORM_BASE_URL="https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION"
+TERRAFORM_ZIP_NAME="terraform_0.10.2_linux_amd64.zip?_ga=2.223865688.278059054.1504047112-1151940089.1504047112"
+TERRAFORM_DOWNLOAD_URL="$TERRAFORM_BASE_URL/$TERRAFORM_ZIP_NAME"
+TERRAFORM_INSTALL_PATH="/usr/local/terraform"
+TERRAFORM_FINAL_BIN_PATH="$TERRAFORM_INSTALL_PATH/terraform-$TERRAFORM_VERSION"
+
 echo ""
 echo "[Provision-Script] Installing Terraform"
 echo ""
 
-# -- this is a work in progess! --
+# Create terraform home
+mkdir -p "$TERRAFORM_INSTALL_PATH"
 
-#cd /root
-#rm -rf awscli-bundle
-#rm -f awscli-bundle.zip
+# .. and go there
+cd "$TERRAFORM_INSTALL_PATH"
 
-#curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-#unzip awscli-bundle.zip
-#./awscli-bundle/install -i /usr/local/aws -b /usr/bin/aws
+# Download
+wget "$TERRAFORM_DOWNLOAD_URL"
 
-#https://releases.hashicorp.com/terraform/0.10.2/terraform_0.10.2_linux_amd64.zip?_ga=2.151879802.278059054.1504047112-1151940089.1504047112
+# Rename the zip file
+mv "$TERRAFORM_ZIP_NAME" "terraform.zip"
+
+# Unzip the zip file
+unzip "terraform.zip"
+
+# Rename binary
+mv "terraform" "terraform-$TERRAFORM_VERSION"
+
+# Create symlink for 'terraform'
+rm -f "/usr/sbin/terraform"
+ln -s "$TERRAFORM_FINAL_BIN_PATH" "/usr/sbin/terraform"
+
+# Create symlink for 'tf'
+rm -f "/usr/sbin/tf"
+ln -s "$TERRAFORM_FINAL_BIN_PATH" "/usr/sbin/tf"
+
+# Finalize Permissions
+chown root:root -R "$TERRAFORM_INSTALL_PATH"
+chmod 0755 -R "$TERRAFORM_INSTALL_PATH"
+
+echo ""
+echo "[Provision-Script] Installing Terraform"
+echo ""
+echo "    > Installed $(tf --version)"
+echo ""
+
