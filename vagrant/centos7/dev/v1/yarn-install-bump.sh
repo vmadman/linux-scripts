15942d6d4bbf9a4e2dcb74a95520fa5171531a0e
Luke Chavers <vmadman@gmail.com>
2017-10-25 11:20:52 -0500

    Create yarn-install-bump.sh

---
+++ vagrant/centos7/dev/v1/yarn-install-bump.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script uses Yarn to install 'bump' globally
+# see: https://github.com/kilianc/node-bump
+
+echo ""
+echo "[Provision-Script] Installing Bump (via Yarn)"
+echo ""
+
+yarn global add bump
