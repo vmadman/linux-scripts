be9fd837decebda3c008586784e2d022771c0fe0
Luke Chavers <vmadman@gmail.com>
2017-10-25 11:43:48 -0500

    Update yum-install-yarn.sh

--- vagrant/centos7/dev/v1/yum-install-yarn.sh
+++ vagrant/centos7/dev/v1/yum-install-yarn.sh
@@ -10,3 +10,25 @@ echo ""
 wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 yum install yarn -y
+
+# --
+
+echo " "
+echo "[Provision-Script] Adding Yarn Binaries to PATH.."
+echo " "
+
+YARN_BINARY_PATH="/usr/local/bin"
+
+add_yarn_root_path() {
+
+    TARGET_BASH_RC="$1"
+    echo "" >> "$TARGET_BASH_RC"
+    echo "# Add the Yarn binary root path to PATH" >> "$TARGET_BASH_RC"
+    echo "export PATH="'"$PATH:'"$YARN_BINARY_PATH"'"' >> "$TARGET_BASH_RC"
+
+    echo "          at: $TARGET_BASH_RC"
+
+}
+
+add_yarn_root_path "/root/.bashrc"
+add_yarn_root_path "/home/vagrant/.bashrc"
