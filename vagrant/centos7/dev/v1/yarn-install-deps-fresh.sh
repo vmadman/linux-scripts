ff13e07385a3d4dcb5aa6c8455c8d4ed7d6f5731
Luke Chavers <vmadman@gmail.com>
2017-11-03 19:19:23 -0500

    Create yarn-install-deps-fresh.sh

---
+++ vagrant/centos7/dev/v1/yarn-install-deps-fresh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script removes any existing "node_modules" and then 
+# executes "yarn install" on the current, active, project 
+# (which is assumed to be located at /project)
+
+echo ""
+echo "[Provision-Script] Removing existing 'node_modules' ..."
+echo ""
+
+cd /project
+rm -rf ./node_modules
+
+echo ""
+echo "[Provision-Script] Installing Project Dependencies (w/Yarn)"
+echo ""
+
+yarn install
